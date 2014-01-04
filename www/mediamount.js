47563c5e059ea9ea1d0a16a99742b0f29a5a16d8
Gabor Tomasi <tomasi@gabors-mbp.fritz.box>
2014-01-04 18:26:57 +0100

    first commit

---
+++ www/mediamount.js
@@ -0,0 +1,12 @@
+var mediamount = {
+	createEvent: function(successCallback, errorCallback) {
+		cordova.exec(
+			successCallback, // success callback function
+			errorCallback, // error callback function
+			'MediaMount', // mapped to our native Java class called "CalendarPlugin"
+			'update', // with this action name
+			[]
+		);
+	}
+}
+module.exports = mediamount;
