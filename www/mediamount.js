a8385a250c173eaca976fb8cd9caf2c7a28ce4ba
Gabor Tomasi <tomasi@Gabors-MacBook-Pro.local>
2014-01-06 17:02:58 +0100

    Copied other structure!

--- www/mediamount.js
+++ www/mediamount.js
@@ -1,12 +1,12 @@
 var MediaMount = function() {};
-MediaMount.prototype.update = function(successCallback, errorCallback) {
+MediaMount.prototype.update = function(file, successCallback, errorCallback) {
 	console.log('About to update');
 	cordova.exec(
 		successCallback, // success callback function
 		errorCallback, // error callback function
 		'MediaMount', // mapped to our native Java class called "CalendarPlugin"
 		'update',// with this action name
-		[]
+		[{'file': file}]
 	);
 };
 
