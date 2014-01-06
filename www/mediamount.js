4ddbe3f64abbe3abb8413be438e22dc0b6ed5e08
Gabor Tomasi <tomasi@Gabors-MacBook-Pro.local>
2014-01-06 16:28:32 +0100

    Copied other structure!

--- www/mediamount.js
+++ www/mediamount.js
@@ -1,11 +1,15 @@
-var mediamount = {
-    createEvent: function(successCallback, errorCallback) {
-        cordova.exec(
-            successCallback, // success callback function
-            errorCallback, // error callback function
-            'MediaMount', // mapped to our native Java class called "CalendarPlugin"
-            'update'// with this action name
-        ); 
-    }
-}
-module.exports = mediamount;
+var MediaMount = {};
+MediaMount.prototype.update = function() {
+	cordova.exec(
+		successCallback, // success callback function
+		errorCallback, // error callback function
+		'MediaMount', // mapped to our native Java class called "CalendarPlugin"
+		'update'// with this action name
+	);
+};
+
+cordova.addConstructor(function() {
+	window.mediaMount = new MediaMount();
+	return window.mediaMount;
+});
+
