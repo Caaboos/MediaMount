08a01cf1da37ed77f7bf073fc18647ad124960b8
Gabor Tomasi <tomasi@Gabors-MacBook-Pro.local>
2014-01-06 16:43:09 +0100

    Copied other structure!

--- src/android/MediaMount.java
+++ src/android/MediaMount.java
@@ -1,13 +1,14 @@
 package com.caaboos.mediamount;
- 
+
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
 import org.json.JSONObject;
 import org.json.JSONArray;
 import org.json.JSONException;
 
-import android.app.Activity;
 import android.content.Intent;
+import android.os.Environment;
+import android.net.Uri;
 
 public class MediaMount extends CordovaPlugin {
     public static final String UPDATE = "update";
@@ -16,6 +17,9 @@ public class MediaMount extends CordovaPlugin {
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
         try {
             if (UPDATE.equals(action)) {
+                //sendBroadcast(new Intent(Intent.ACTION_MEDIA_MOUNTED, Uri.parse("file://" + Environment.getExternalStorageDirectory())));
+
+                this.cordova.getActivity().sendBroadcast(new Intent(Intent.ACTION_MEDIA_MOUNTED, Uri.parse("file://" + Environment.getExternalStorageDirectory())));
 
                callbackContext.success();
                return true;
